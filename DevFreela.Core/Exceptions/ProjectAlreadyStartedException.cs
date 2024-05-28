5b7f9a7542307696fc70dfd584924688dacd75fd
Thiago <thiagooffice2@gmail.com>
2024-05-28 09:20:10 -0300

    feat: creating Core layer. entities base, project, projectComment, skill, user, userskill. Enums, Exceptions and Reposities.

---
+++ DevFreela.Core/Exceptions/ProjectAlreadyStartedException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DevFreela.Core.Exceptions
+{
+    public class ProjectAlreadyStartedException : Exception
+    {
+        public ProjectAlreadyStartedException() : base("Project alerady in Started status")
+        {
+            
+        }
+    }
+}
