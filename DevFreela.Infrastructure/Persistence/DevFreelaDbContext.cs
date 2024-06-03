901787f4c7b9c197977b8c078dd6b1653cd4ad3e
Thiago <thiagooffice2@gmail.com>
2024-06-03 19:44:47 -0300

    feat: config the entities, dbContext and configurations persistence

--- DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
+++ DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
@@ -1,35 +1,26 @@
 ﻿using DevFreela.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace DevFreela.Infrastructure.Persistence
 {
-    public class DevFreelaDbContext
+    public class DevFreelaDbContext : DbContext
     {
-        public DevFreelaDbContext()
+        public DevFreelaDbContext(DbContextOptions<DevFreelaDbContext> options) : base(options)
         {
-            Projects = new List<Project>
-            {
-                new Project("Meu projeto aspnet core 1", "Minha descrição 1", 1, 1, 10000),
-                new Project("Meu projeto aspnet core 2", "Minha descrição 2", 1, 1, 20000),
-                new Project("Meu projeto aspnet core 3", "Minha descrição 3", 1, 1, 30000)
-            };
-            Users = new List<User>
-            {
-                new User("Thiago leal", "thiago@gmail.com", new DateTime(1999,1,1)),
-                new User("Roberto leal", "roberto@gmail.com", new DateTime(2003,1,1)),
-                new User("Victor leal", "victor@gmail.com", new DateTime(1996,1,1)),
-            };
-            Skills = new List<Skill>
-            {
-                new Skill(".NET Core"),
-                new Skill("React"),
-                new Skill("AWS")
-            };
+            
         }
-        public List<Project> Projects { get;  set; }
-        public List<User> Users { get;  set; }
-        public List<Skill> Skills { get;  set; }
-        public List<ProjectComment> ProjectComments { get; set; }
+        public DbSet<Project> Projects { get;  set; }
+        public DbSet<User> Users { get;  set; }
+        public DbSet<Skill> Skills { get;  set; }
+        public DbSet<UserSkill> UserSkills { get; set; }
+        public DbSet<ProjectComment> ProjectComments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
