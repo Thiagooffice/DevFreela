615161bbcc09a3d6158efc54f0f97fb44efde5b8
Thiago <thiagooffice2@gmail.com>
2024-06-11 16:54:22 -0300

    feat: creating loginUserCommand and handler with login functionality

---
+++ DevFreela.Application/Commands/LoginUser/LoginUserCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using DevFreela.Application.ViewModels;
+using DevFreela.Core.Repositories;
+using DevFreela.Core.Services;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Commands.LoginUser
+{
+    public class LoginUserCommandHandler : IRequestHandler<LoginUserCommand, LoginUserViewModel>
+    {
+        private readonly IAuthService _authService;
+        private readonly IUserRepository _userRepository;
+        public LoginUserCommandHandler(IAuthService authService, IUserRepository userRepository)
+        {
+            _authService = authService;
+            _userRepository = userRepository;
+        }
+        public async Task<LoginUserViewModel> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+        {
+            // Utilizar o mesmo algoritmo para criar o hash dessa.
+            var passwordHash = _authService.ComputeSha256Hash(request.Password);
+
+            // Buscar no meu banco de dados um user que tenha meu email e minha senha em formato de hash.
+            var user = await _userRepository.GetUserByEmailAndPasswordAsync(request.Email, passwordHash);
+
+            // Se nao existir, erro no login.
+            if(user == null)
+            {
+                return null;
+            }
+
+            // Se existir, gero o token usando os dados do usuario.
+            var token = _authService.GenerateJwtToken(user.Email, user.Role);
+
+            return new LoginUserViewModel(user.Email, token);
+        }
+    }
+}
