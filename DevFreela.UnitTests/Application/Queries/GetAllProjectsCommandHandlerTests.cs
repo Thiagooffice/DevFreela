db83ef8d806603f49acc64332125fc49f1d02fe9
Thiago <thiagooffice2@gmail.com>
2024-06-13 11:38:23 -0300

    feat: adding tests. Creating new project for test, adding xUnit, following AAA and Given-When-Then pattern, adding Moq.

---
+++ DevFreela.UnitTests/Application/Queries/GetAllProjectsCommandHandlerTests.cs
@@ -0,0 +1,45 @@
+﻿using DevFreela.Application.Queries.GetAllProject;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DevFreela.UnitTests.Application.Queries
+{
+    public class GetAllProjectsCommandHandlerTests
+    {
+        [Fact]
+        //Given When Then
+        public async Task ThreeProjectsExist_Executed_ReturnThreeProjectViewModels()
+        {
+            // Arrange
+            var projects = new List<Project>
+            {
+                new Project("Nome do Teste", "Descrição de Teste", 1, 2, 10000),
+                new Project("Nome do Teste 2", "Descrição de Teste 2", 1, 2, 20000),
+                new Project("Nome do Teste 3", "Descrição de Teste 3", 1, 2, 30000)
+            };
+
+            var projectRepositoryMock = new Mock<IProjectRepository>();
+            projectRepositoryMock.Setup(pr => pr.GetAllAsync().Result).Returns(projects);
+
+            var getAllProjectsQuery = new GetAllProjectsQuery("");
+            var getAllProjectsQueryHandler = new GetAllProjectsQueryHandler(projectRepositoryMock.Object);
+
+            //Act
+            var projectViewModelList = await getAllProjectsQueryHandler.Handle(getAllProjectsQuery, new System.Threading.CancellationToken());
+
+            //Assert
+            Assert.NotNull(projectViewModelList);
+            Assert.NotEmpty(projectViewModelList);
+            Assert.Equal(projects.Count, projectViewModelList.Count);
+
+            projectRepositoryMock.Verify(pr => pr.GetAllAsync().Result, Times.Once);
+        }
+    }
+}
